106d3eec13fcd822333a8ef45f5a1931741ef575
mateuszzieba <mz@cvt.services>
2025-04-06 15:51:01 +0200

    fix source img

--- chess_api/urls.py
+++ chess_api/urls.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from rest_framework.routers import DefaultRouter
-from django.urls import include, re_path
+from django.urls import include, re_path, path
 from chess_api.views import GameViewSet
-
+def health(request):
+    return HttpResponse("OK", status=200)
 default_router = DefaultRouter()
 
 default_router.register(r"chess_game", GameViewSet, basename="chess_game")
@@ -11,4 +13,5 @@ api_urls += default_router.urls
 
 urlpatterns = [
     re_path(r"api/", include(api_urls)),
+    path('health', health),
 ]
